e9f1f572cfdf002d55de1c582cbf6052047fe2b9
Mikhail <mikko.deriabin@gmail.com>
2023-04-17 23:41:56 +0300

    add /esp32: create and read values of esp32 device

--- influxHandler/influxHandler.js
+++ influxHandler/influxHandler.js
@@ -1,23 +1,27 @@
 const {Point, InfluxDB, HttpError} = require("@influxdata/influxdb-client");
-const {SensorValue} = require("../MQTT/sensorValue");
 
 class InfluxHandler {
+    constructor() {
+       this.influxObj = new InfluxDB({url: this.#url, token: this.#token});
+    }
+
+    influxObj;
+
     #url = process.env.INFLUX_URL;
     #token = process.env.INFLUX_TOKEN;
     #org = process.env.INFLUX_ORGANIZATION;
-    #bucket = process.env.INFLUX_ESP86_BUCKET_ID;
+    #bucket = process.env.INFLUX_ESP32_BUCKET_ID;
 
     write = async (obj) => {
         const point = new Point('weather')
-            .tag('station', 'esp86')
-            .floatField(SensorValue[0], obj[SensorValue[0]])
-            .floatField(SensorValue[1], obj[SensorValue[1]])
+            .tag('station', 'esp32')
+            .floatField('temp', obj['temp'])
             .timestamp(new Date().getTime());
 
-        const writeApi = new InfluxDB({url: this.#url, token: this.#token}).getWriteApi(this.#org, this.#bucket, 'ms');
-        writeApi.writePoint(point);
+        const writeAPI = this.influxObj.getWriteApi(this.#org, this.#bucket, 'ms');
+        writeAPI.writePoint(point);
         try {
-            await writeApi.close();
+            await writeAPI.close();
         } catch (e) {
             if (e instanceof HttpError) {
                 console.error(e.statusCode);
@@ -25,6 +29,28 @@ class InfluxHandler {
             } else
                 console.log(e);
         }
+    }
+
+    readAll = async () => {
+        //last 100 days data
+        const fluxTempQuery = `from(bucket:"esp32") |> range(start: -100d) |> filter(fn: (r) => r._field == "temp")`;
+        const readAPI = await this.influxObj.getQueryApi(this.#org);
+
+        const result = await readAPI.collectRows(
+            fluxTempQuery
+        )
+
+        try {
+            readAPI.close();
+        } catch (e) {
+            if (e instanceof HttpError) {
+                console.error(e.statusCode);
+                console.error(e.statusMessage);
+            } else
+                console.log(e);
+        }
+
+        return result;
     }
 }
 
--- server.js
+++ server.js
@@ -3,29 +3,23 @@ require("dotenv").config({path: './.env'});
 const express = require('express');
 const cors = require('cors');
 
-const {Sensor} = require('./MQTT/sensor');
-const {File} = require('./dataFile/file');
-const {InfluxHandler} = require("./influxHandler/influxHandler");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors({credentials: true, origin: process.env.CORS_ORIGIN}));
 
 app.use("/esp86", require('./esp86/esp86Router'));
-
-const sensor = new Sensor('1/0');
-const file = new File('./data');
-const influx = new InfluxHandler();
+app.use("/esp32", require('./esp32/esp32.router'));
 
 app.listen(process.env.SERVER_PORT, async () => {
     displayLinks();
+    /*
     setInterval(async () => {
         const respObj = await sensor.measure();
         await influx.write(respObj);
 
         await file.saveData('esp86.json', respObj);
-    }, 15000);
+    }, 15000);*/
 });
 
 function displayLinks(){
